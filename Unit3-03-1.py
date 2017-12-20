9002b065f35b91d96acf2804b235308afda69ede
Peter  Zhu <peter.zhu@ocsbstudent.ca>
2017-12-19 23:19:16 -0500

    init

---
+++ Unit3-03-1.py
@@ -0,0 +1,62 @@
+# Created by: Peter Zhu
+# Created on: Sep-19
+# Created for: ICS3U
+# This is a program 
+
+import ui
+
+from numpy import random
+
+computer_op = random.randint(1,3)
+
+user_ans = None
+computer_ans = None
+
+if (computer_op == 1) :
+     computer_ans = 'Rock'
+elif (computer_op == 2):
+     computer_ans = 'Paper'
+elif   (computer_op == 3):
+     computer_ans = 'Scissors'
+
+def rock_touch_up_inside (sender):
+    global user_ans 
+    user_ans = str('Rock')
+    check_answer()
+
+def paper_touch_up_inside (sender):
+    global user_ans
+    user_ans = str('Paper')
+    check_answer()
+    
+def scissors_touch_up_inside (sender):
+    global user_ans
+    user_ans = str('Scissors')
+    check_answer()
+
+def check_answer ():
+    print(computer_ans)
+    print(user_ans)
+    
+    if computer_ans == str('Rock') and user_ans == str('Rock'):
+        view['output_label'].text = ("It's a tie!")
+    elif computer_ans == str('Rock') and user_ans == str('Paper'):
+        view['output_label'].text = ("You win")
+    elif computer_ans == str('Rock') and user_ans == str('Scissors'):
+        view['output_label'].text = ("You have lost")
+    elif computer_ans == str('Paper') and user_ans == str('Paper'):
+        view['output_label'].text = ("It's a tie!")
+    elif computer_ans == str('Paper') and user_ans == str('Rock'):
+    	
+        view['output_label'].text = ("You have lost")
+    elif computer_ans == str('Paper') and user_ans == str('Scissors'):
+        view['output_label'].text = ("You win")
+    elif computer_ans == str('Scissors') and user_ans == str('Scissors'):
+        view['output_label'].text = ("It's a tie!")
+    elif computer_ans == str('Scissors') and user_ans == str('Paper'):
+        view['output_label'].text = ("You have lost")
+    elif computer_ans == str('Scissors') and user_ans == str('Scissors'):
+        view['output_label'].text = ("You win!")
+
+view = ui.load_view()
+view.present('sheet')
